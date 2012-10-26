477c8a5f17df3755050efa1b8196c644924c1456
matthias.krompass@gmail.com <matthias.krompass@gmail.com@aa56c8aa-4685-0f71-9b7c-26d792141b0b>
2012-10-26 21:15:13 +0000

    mitglieder-verwaltung: import

--- modules/vvb/mitglieder-functions.inc.php
+++ modules/vvb/mitglieder-functions.inc.php
@@ -61,7 +61,8 @@
                    $specialvars["crypt_meth"],
                    $specialvars["crypt_salt"],
                    $string,
-                   $specialvars["crypt_mode"]
+                   $specialvars["crypt_mode"],
+                   $specialvars["crypt_salt"]
                );
              }
 
@@ -76,11 +77,54 @@
                    $specialvars["crypt_meth"],
                    $specialvars["crypt_salt"],
                    $string,
-                   $specialvars["crypt_mode"]
+                   $specialvars["crypt_mode"],
+                   $specialvars["crypt_salt"]
                );
              }
 
          }
+         
+         
+         // http://fluuux.de/2012/07/datensatze-verschlusselt-in-datenbank-speichern/
+         class Encryption {
+            var $skey = PASSPHRASE;
+
+            public  function safe_b64encode($string) {
+
+                $data = base64_encode($string);
+                $data = str_replace(array('+','/','='),array('-','_',''),$data);
+                return $data;
+            }
+
+            public function safe_b64decode($string) {
+                $data = str_replace(array('-','_'),array('+','/'),$string);
+                $mod4 = strlen($data) % 4;
+                if ($mod4) {
+                    $data .= substr('====', $mod4);
+                }
+                return base64_decode($data);
+            }
+
+            public  function encode($value){
+
+                if(!$value){return false;}
+                $text = $value;
+                $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+                $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+                $crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->skey, $text, MCRYPT_MODE_ECB, $iv);
+                return trim($this->safe_b64encode($crypttext));
+            }
+
+            public function decode($value){
+
+                if(!$value){return false;}
+                $crypttext = $this->safe_b64decode($value);
+                $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+                $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+                $decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->skey, $crypttext, MCRYPT_MODE_ECB, $iv);
+                return trim($decrypttext);
+            }
+        }
     }
 
     ### platz fuer weitere funktionen ###
