7b82cd61989a604394eabf816f45901a6639bfa0
Krompi <krompi@gmail.com>
2015-10-02 17:44:43 +0200

    symlinks angepaßt

--- symlinks.sh
+++ symlinks.sh
@@ -27,59 +27,59 @@ exit;
 
 
 cd ..
-cd ewebuki
+cd ${path_ewebuki}
 
-ln -sfv ../vvb/.htaccess
+ln -sfv ../${path_custom}/.htaccess
 rm -rf  file
-ln -sfv ../vvb/file
+ln -sfv ../${path_custom}/file
 
 # Ordner Links setzen
 cd css/
     ln -sfv ../../vvb/css/vvb
-    ln -sfv ../../vvb/css/vvb_01
+    ln -sfv ../../${path_custom}/css/vvb_01
 cd ..
 
 cd images
-    ln -sfv ../../vvb/images/vvb
+    ln -sfv ../../${path_custom}/images/vvb
 cd ..
 
 cd js
-    ln -sfv ../../vvb/js/vvb
+    ln -sfv ../../${path_custom}/js/vvb
 cd ..
 cd modules
-    ln -sfv ../../vvb/modules/vvb
+    ln -sfv ../../${path_custom}/modules/vvb
 cd ..
 
 cd templates
-    ln -sfv ../../vvb/templates/vvb
-    ln -sfv ../../vvb/templates/vvb_01
+    ln -sfv ../../${path_custom}/templates/vvb
+    ln -sfv ../../${path_custom}/templates/vvb_01
 cd ..
 
 # configs-Links setzen
 cd conf
-    ln -sfv ../../vvb/conf/auth.cfg.php
-    ln -sfv ../../vvb/conf/file.cfg.php
-    ln -sfv ../../vvb/conf/modules.cfg.php
-    ln -sfv ../../vvb/conf/overwrite.cfg.php
-    ln -sfv ../../vvb/conf/site.cfg.php
+    ln -sfv ../../${path_custom}/conf/auth.cfg.php
+    ln -sfv ../../${path_custom}/conf/file.cfg.php
+    ln -sfv ../../${path_custom}/conf/modules.cfg.php
+    ln -sfv ../../${path_custom}/conf/overwrite.cfg.php
+    ln -sfv ../../${path_custom}/conf/site.cfg.php
 cd ..
 
 cd modules
     cd basic
-        ln -sfv ../../../vvb/modules/basic/menu2.cfg.php
-        ln -sfv ../../../vvb/modules/basic/path.cfg.php
+        ln -sfv ../../../${path_custom}/modules/basic/menu2.cfg.php
+        ln -sfv ../../../${path_custom}/modules/basic/path.cfg.php
     cd ..
     cd addon
-        ln -sfv ../../../vvb/modules/addon/kontakt.cfg.php
-        ln -sfv ../../../vvb/modules/addon/changed.cfg.php
+        ln -sfv ../../../${path_custom}/modules/addon/kontakt.cfg.php
+        ln -sfv ../../../${path_custom}/modules/addon/changed.cfg.php
     cd ..
     cd admin
-        ln -sfv ../../../vvb/modules/admin/bloged.cfg.php
-        ln -sfv ../../../vvb/modules/admin/contented.cfg.php
-        ln -sfv ../../../vvb/modules/admin/menued2.cfg.php
-        ln -sfv ../../../vvb/modules/admin/fileed2.cfg.php
+        ln -sfv ../../../${path_custom}/modules/admin/bloged.cfg.php
+        ln -sfv ../../../${path_custom}/modules/admin/contented.cfg.php
+        ln -sfv ../../../${path_custom}/modules/admin/menued2.cfg.php
+        ln -sfv ../../../${path_custom}/modules/admin/fileed2.cfg.php
     cd ..
     cd wizard
-        ln -sfv ../../../vvb/modules/wizard/wizard.cfg.php
+        ln -sfv ../../../${path_custom}/modules/wizard/wizard.cfg.php
     cd ..
 cd ..
